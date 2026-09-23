661946143d47a6bf04154c64e0285bdf29a0f553
Zach Kelling <z@zeekay.io>
2026-09-23 02:39:37 -0700

    Drop retired testnets from the client
    Goerli, Rinkeby, Ropsten, Mumbai, Optimism Goerli and Arbitrum Goerli
    have been shut down. The client keeps the local anvil chain and Sepolia.
    
    Co-authored-by: Hanzo Dev <dev@hanzo.ai>

--- client/src/constants.js
+++ client/src/constants.js
@@ -16,34 +16,10 @@ export const NETWORKS = {
     url: "http://localhost",
     port: 8545,
   },
-  GOERLI: {
-    name: "goerli",
-    id: "5",
-    url: `${process.env.GOERLI_HOST}`,
-    privKey: `${process.env.PRIV_KEY}`,
-  },
-  MUMBAI: {
-    name: "mumbai-polygon",
-    id: "80001",
-    url: `${process.env.MUMBAI_HOST}`,
-    privKey: `${process.env.PRIV_KEY}`,
-  },
   SEPOLIA: {
     name: "sepolia",
     id: "11155111",
     url: `${process.env.SEPOLIA_HOST}`,
-    privKey: `${process.env.PRIV_KEY}`,
-  },
-  OPTIMISM_GOERLI: {
-    name: "goerli-optimism",
-    id: "420",
-    url: `${process.env.OPTIMISM_GOERLI_HOST}`,
-    privKey: `${process.env.PRIV_KEY}`,
-  },
-  ARBITRUM_GOERLI: {
-    name: "goerli-arbitrum",
-    id: "421613",
-    url: `${process.env.ARBITRUM_GOERLI_HOST}`,
     privKey: `${process.env.PRIV_KEY}`,
   },
 };
@@ -57,30 +33,6 @@ export const NETWORKS_INGAME = {
     url: "http://localhost",
     port: 8545,
   },
-  GOERLI: {
-    name: "goerli",
-    id: "5",
-    currencyName: "Goerli-ETH",
-    currencySymbol: "ETH",
-    rpcUrl: `https://eth-goerli.public.blastapi.io`,
-    blockExplorer: "https://goerli.etherscan.io",
-    explorer: {
-      apiKey: `${process.env.REACT_APP_GOERLI_EXPLORER_API_KEY}`,
-      apiHost: `https://api-goerli.etherscan.io`,
-    },
-  },
-  MUMBAI: {
-    name: "mumbai-polygon",
-    id: "80001",
-    currencyName: "Mumbai-Matic",
-    currencySymbol: "MATIC",
-    rpcUrl: `https://matic-mumbai.chainstacklabs.com`,
-    blockExplorer: "https://mumbai.polygonscan.com",
-    explorer: {
-      apiKey: `${process.env.REACT_APP_MUMBAI_EXPLORER_API_KEY}`,
-      apiHost: `https://api-testnet.polygonscan.com`,
-    },
-  },
   SEPOLIA: {
     name: "sepolia",
     id: "11155111",
@@ -93,51 +45,11 @@ export const NETWORKS_INGAME = {
       apiHost: `https://api-sepolia.etherscan.io`,
     },
   },
-  OPTIMISM_GOERLI: {
-    name: "goerli-optimism",
-    id: "420",
-    currencyName: "Optimism-ETH",
-    currencySymbol: "ETH",
-    rpcUrl: `https://goerli.optimism.io`,
-    blockExplorer: "https://goerli-optimism.etherscan.io",
-    explorer: {
-      apiKey: `${process.env.REACT_APP_OPTIMISM_GOERLI_EXPLORER_API_KEY}`,
-      apiHost: `https://api-goerli-optimistic.etherscan.io`,
-    },
-  },
-  ARBITRUM_GOERLI: {
-    name: "goerli-arbitrum",
-    id: "421613",
-    currencyName: "Arbitrum-ETH",
-    currencySymbol: "ETH",
-    rpcUrl: `https://goerli-rollup.arbitrum.io/rpc`,
-    blockExplorer: "https://goerli.arbiscan.io",
-    explorer: {
-      apiKey: `${process.env.REACT_APP_ARBITRUM_GOERLI_EXPLORER_API_KEY}`,
-      apiHost: `https://api-goerli.arbiscan.io`,
-    },
-  },
 };
 
 // Deprectated networks
 // status: {deprecated | deprecation-planned}
-export const NETWORKS_DEPRECATION = {
-  GOERLI: {
-    id: "5",
-    status: "deprecation-planned",
-    date: "jan-2024"
-  },
-  OPTIMISM_GOERLI: {
-    id: "420",
-    status: "deprecation-planned",
-    date: "jan-2024"
-  },
-  ARBITRUM_GOERLI: {
-    id: "421613",
-    status: "deprecation-planned",
-    date: "jan-2024"
-  }
-};
+export const NETWORKS_DEPRECATION = {};
 
 // Web3
 export const Web3 = pkgWeb3;
@@ -149,17 +61,11 @@ export const GOOGLE_ANALYTICS_ID = "UA-85043059-4";
 // Owner addresses
 export const ADDRESSES = {
   [NETWORKS.LOCAL.name]: undefined,
-  [NETWORKS.MUMBAI.name]: "0x09902A56d04a9446601a0d451E07459dC5aF0820",
-  [NETWORKS.GOERLI.name]: "0x09902A56d04a9446601a0d451E07459dC5aF0820",
   [NETWORKS.SEPOLIA.name]: "0x09902A56d04a9446601a0d451E07459dC5aF0820",
-  [NETWORKS.OPTIMISM_GOERLI.name]: "0x09902A56d04a9446601a0d451E07459dC5aF0820",
-  [NETWORKS.ARBITRUM_GOERLI.name]: "0x09902A56d04a9446601a0d451E07459dC5aF0820",
 };
 
 // Support EIP-1559
 export const SUPPORTS_EIP_1559 = [
-  '5', // NETWORKS.GOERLI.id
-  '80001', // NETWORKS.MUMBAI.id
   '11155111', // NETWORKS.SEPOLIA.id
 ]
 
@@ -193,10 +99,6 @@ export const SHOW_ALL_COMPLETE_DESCRIPTIONS = false; /* FALSE on production */
 export const SHOW_VERSION = true;
 
 // export const ACTIVE_NETWORK = NETWORKS.SEPOLIA
-// export const ACTIVE_NETWORK = NETWORKS.GOERLI
-// export const ACTIVE_NETWORK = NETWORKS.MUMBAI
-// export const ACTIVE_NETWORK = NETWORKS.OPTIMISM_GOERLI
-// export const ACTIVE_NETWORK = NETWORKS.ARBITRUM_GOERLI
 // export const ACTIVE_NETWORK = NETWORKS.LOCAL;
 
 let id_to_network = {};
